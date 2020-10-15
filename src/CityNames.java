353f523629f24d4f1c4858769bf7d23585301f58
mell2262 <mellgomez.2262@gmail.com>
2020-10-15 19:09:13 +0200

    fase2

--- src/CityNames.java
+++ src/CityNames.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 
 public class CityNames {
@@ -5,12 +8,12 @@ public class CityNames {
 	public static void main(String[] args) {
 		/*Ejercicio: El ejercicio consiste en mostrar por consola diferentes nombres de ciudades partiendo de un 
 		 * array y modificar los nombres cambiando letras.
-		FASE 1
-		- Crea seis variables tipu string vacías.
-		- Pide por consola que se introduzcan los nombres.
-		- Introduce los siguientes nombres de ciudades (Barcelona, Madrid, Valencia, Málaga, Cádiz, Santander) para 
-		teclado.
-		- Mostrar por consola el nombre de las 6 ciudades.
+		FASE 2
+		- Una vez tenemos los nombres de las ciudades guardados en variables tendremos que pasar la información a un 
+		array (arrayCiutats).
+		- Cuando tengamos el array lleno, tendremos que mostrar por consola el nombre de las ciudades ordenadas por 
+		orden alfabético.
+
 		 */ 
 		
 		String varUno, varDos, varTres, varCuatro, varCinco, varSeis;
@@ -24,10 +27,18 @@ public class CityNames {
 		varCuatro=scan.nextLine();
 		varCinco=scan.nextLine();
 		varSeis=scan.nextLine();
-		System.out.println(varUno+", "+ varDos+", "+ varTres+", " +varCuatro+", "+ varCinco+", "+ varSeis);
-		
+		//System.out.println(varUno+", "+ varDos+", "+ varTres+", " +varCuatro+", "+ varCinco+", "+ varSeis);
 		
 		scan.close();
+		
+		
+		List<String> arrayCities= Arrays.asList(varUno,varDos,varTres,varCuatro,varCinco,varSeis);
+		Collections.sort(arrayCities);
+		for (String i:arrayCities) {
+			System.out.println(i);
+		}
+		
+		
 	}
 
 }
